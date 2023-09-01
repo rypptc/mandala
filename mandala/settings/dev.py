d495ef4a97cc3e3b5e0e59a1508c03f5634eb235
rypptc <rypptc@gmail.com>
2023-09-01 12:52:39 -0400

    Basic wagtail project with custom user

---
+++ mandala/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7(f0b2l8ubyfieb(&l7$a=(7%rsp)!%2#wcfzjfvg2!h0&t*$%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
